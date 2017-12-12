b2a1dba0544969905162b8d5d1c0a38ddf01b4d9
Dev Dixit <deva_0801@hotmail.com>
2017-12-12 16:53:11 +0000

    Added some XML tests

--- test/dataExports.js
+++ test/dataExports.js
@@ -316,31 +316,43 @@ describe('Filtering function tests:', function(){
     
 });
 
-
-describe('XML TESTS', function(){
+// Run XML based tests
+describe('XML TESTS', function() { 
     describe('XML syntax', function() {
         it('Test 1 - check if it generates XML', function(){
             var obj = {foo:1};
             var expected = "\t<test>" + "\n" + "    <foo>1</foo>" + "\n" + "</test>\n";
-            assert.equal(XMLDataFile.convertObjectToXML(obj, false, false, "test"), expected);
+            assert.equal(CSVConverter.convertObjectToXML(obj, false, false, "test"), expected);
         });
         
 	it('Test 2 - check if it produces doctype', function() {
 	    var obj = {foo:2};
 	    var expected = '\t<?xml version=\'1.0\'?>\n<tests>\n<test>\n    <foo>2</foo>\n</test>\n';
-	    assert.equal(XMLDataFile.convertObjectToXML(obj, true, false, "test"), expected);
+	    assert.equal(CSVConverter.convertObjectToXML(obj, true, false, "test"), expected);
         });
 	
 	it('Test 3 - check if encloses the data object in correct tag', function() {
 	    var obj = {foo:3};
 	    var expected = '\t<test>\n' + '    <foo>3</foo>\n' + '</test>\n';
-	    assert.equal(XMLDataFile.convertObjectToXML(obj, false, false, "test"), expected);
+	    assert.equal(CSVConverter.convertObjectToXML(obj, false, false, "test"), expected);
 	});
 	it('Test 4 - check if the root tag is closed at the end', function() {
 	    var obj = {foo:4};
 	    var expected = '\t<test>\n' + '    <foo>4</foo>\n' + '</test>\n\n' + '</tests>';
-	    assert.equal(XMLDataFile.convertObjectToXML(obj, false, true, "test"), expected);
+	    assert.equal(CSVConverter.convertObjectToXML(obj, false, true, "test"), expected);
 	});
+
+	it('Test 5 - check null is returned when the value is null', function() {
+	    var obj = {foo:null};
+	    var expected = '\t<test>\n' + '    <foo>null</foo>\n' + '</test>\n';
+	    assert.equal(CSVConverter.convertObjectToXML(obj, false, false, "test"), expected);
+	});
+	it('Test 6 - check special characters are escaped', function() {
+	    var obj = {foo:"hello"};
+	    var expected = '\t<test>\n' + '    <foo>hello</foo>\n' + '</test>\n\n';
+	    assert.equal(CSVConverter.convertObjectToXML(obj, false, false, "test"), expected);
+	});
+	
 
    });
 });
